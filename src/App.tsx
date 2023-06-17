60bd7c700e0226a66c43b3c14dd265f5b5d1a7c9
Roger de castro <rogerd023@hotmail.com>
2023-06-17 18:54:53 -0300

    modificações

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import Navbar from './assets/components/estaticos/navbar/Navbar';
 import Footer from './assets/components/estaticos/footer/Footer';
-import {Grid} from '@material-ui/core';
-import './App.css';
+import Login from './paginas/login/Login'
 import Home from './paginas/home/Home';
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-   <>
+
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+      <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} /> 
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/cadastro" element={<CadastroUsuario />} />*/}
+        </Routes>
+      </div>
       <Footer />
-   </>
+    </BrowserRouter>
+
   );
 }
 
